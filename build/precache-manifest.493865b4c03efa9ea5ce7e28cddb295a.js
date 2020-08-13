db5eae553a31313d23faa75068014af323814e04
Mentionlyze <401785815@qq.com>
2020-08-13 10:19:36 +0800

    build

---
+++ build/precache-manifest.493865b4c03efa9ea5ce7e28cddb295a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9db4b61c4812a97babd65be3098da421",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c6a95ad912f2d67ed45c",
+    "url": "./static/css/2.09700f6a.chunk.css"
+  },
+  {
+    "revision": "b056c2c70657e0c608e4",
+    "url": "./static/css/main.cf13db45.chunk.css"
+  },
+  {
+    "revision": "c6a95ad912f2d67ed45c",
+    "url": "./static/js/2.136491fa.chunk.js"
+  },
+  {
+    "revision": "2db6a849239548a98b7bbdb7e39c2942",
+    "url": "./static/js/2.136491fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b056c2c70657e0c608e4",
+    "url": "./static/js/main.e01444f1.chunk.js"
+  },
+  {
+    "revision": "33785606ee4612ffe6c2",
+    "url": "./static/js/runtime-main.a772369b.js"
+  }
+]);
